4f921983f7b1bfa83a397a046772af4f023ef101
caifakun <466411507@qq.com>
2019-11-08 19:20:21 +0800

    首页导入轮播图组件

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,63 @@
 <template>
   <div>
     <!-- 导入头部组件 -->
-    <Header/>
-    首页
-    <Footer/>
+    <Header />
+    <div class="container">
+      <!-- 幻灯片 -->
+      <el-carousel :interval="5000" arrow="always">
+        <el-carousel-item v-for="(item, index) in banners" :key="index">
+          <div
+            class="banner-image"
+            :style="`
+                background:url(${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+          ></div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <!-- 导入脚部组件 -->
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from '@/components/header.vue'
-import Footer from '@/components/footer.vue'
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
 export default {
-  components:{
+  components: {
     Header,
     Footer
+  },
+  data() {
+    return {
+      // 轮播图数据
+      banners: [
+        {
+          url: "http://157.122.54.189:9095/assets/images/th03.jfif"
+        },
+        {
+          url: "http://157.122.54.189:9095/assets/images/th04.jfif"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
 
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>
