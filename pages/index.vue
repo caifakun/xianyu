9038e284f9ca1ee449de21913f6690373f7fadaa
caifakun <466411507@qq.com>
2019-11-08 19:09:25 +0800

    导入头部组件

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
+    <!-- 导入头部组件 -->
+    <Header/>
     首页
   </div>
 </template>
 
 <script>
-
+import Header from '@/components/header.vue'
 export default {
- 
+  components:{
+    Header
+  }
 }
 </script>
 
