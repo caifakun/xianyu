6a6f6c812858c939dc8b3975151d8c03cfea579e
caifakun <466411507@qq.com>
2019-11-09 17:09:58 +0800

    登录验证表单规则

--- components/user/loginForm.vue
+++ components/user/loginForm.vue
@@ -1,90 +1,85 @@
 <template>
-    <el-form 
-        :model="form" 
-        ref="form"
-        :rules="rules" 
-        class="form">
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
+      <el-input placeholder="用户名/手机" v-model="form.username"></el-input>
+    </el-form-item>
 
-        <el-form-item class="form-item" prop="username">
-            <el-input 
-            placeholder="用户名/手机"
-            v-model="form.username">
-            </el-input>
-        </el-form-item>
+    <el-form-item class="form-item" prop="password">
+      <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
+    </el-form-item>
 
-        <el-form-item class="form-item" prop="password">
-            <el-input 
-            placeholder="密码" 
-            type="password"
-            v-model="form.password">
-            </el-input>
-        </el-form-item>
+    <p class="form-text">
+      <nuxt-link to="#">忘记密码</nuxt-link>
+    </p>
 
-        <p class="form-text">
-            <nuxt-link to="#">忘记密码</nuxt-link>
-        </p>
-
-        <el-button 
-        class="submit"
-        type="primary"
-        @click="handleLoginSubmit">
-            登录
-        </el-button>
-    </el-form>
-
+    <el-button class="submit" type="primary" @click="handleLoginSubmit">登录</el-button>
+  </el-form>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            // 表单数据
-            form: {
-                username:'', //进行双向数据绑定
-                password:'' //进行双向数据绑定
-            },
-            // 表单规则
-            rules: {
-                // 验证用户名
-                username:[
-                    {required: true, message: '请输入用户名/手机',trigger: 'blur'}, 
-                    {min: 11, max: 11,trigger: "blur"}
-                ],
-                // 验证密码
-                password:[
-                    {required:true,message:'请输入密码',trigger: 'blur'},
-                ]
-            },
+  data() {
+    return {
+      // 表单数据
+      form: {
+        username: "", //进行双向数据绑定
+        password: "" //进行双向数据绑定
+      },
+      // 表单规则
+      rules: {
+        // 验证用户名
+        username: [
+          { required: true, message: "请输入用户名/手机", trigger: "blur" },
+        //   { min: 11, max: 11, trigger: "blur" }  这个是验证位数
+        ],
+        // 验证密码
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    // 提交登录
+    handleLoginSubmit() {
+      // 发送ajax请求登录时候，先判断表单信息
+      this.$refs.form.validate(valid => {
+        //   如果valid为ture
+        if (valid) {
+          this.$axios({
+            url: "/accounts/login",
+            method: "post",
+            data: this.form
+          }).then(res => {
+            console.log(res.data);
+          })
+        //   这里用于捕捉失败
+        //   .catch(res=>{
+        //       console.log(res.response)
+        //   })
         }
-    },
-    methods: {
-        // 提交登录
-        handleLoginSubmit(){
-           console.log(this.form);
-           
-        }
+      });
     }
-}
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .form{
-        padding:25px;
-    }
+.form {
+  padding: 25px;
+}
 
-    .form-item{
-        margin-bottom:20px;
-    }
+.form-item {
+  margin-bottom: 20px;
+}
 
-    .form-text{
-        font-size:12px;
-        color:#409EFF;
-        text-align: right;
-        line-height: 1;
-    }
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
 
-    .submit{
-        width:100%;
-        margin-top:10px;
-    }
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
 </style>
