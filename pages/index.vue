35d9f3d0ccc22b19cd54507ac7bbf9ce4487dad3
caifakun <466411507@qq.com>
2019-11-09 16:01:50 +0800

    设置tab栏和搜索框 并渲染数据

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="content">
     <!-- 导入头部组件 -->
     <Header />
     <div class="container">
@@ -15,6 +15,24 @@
           ></div>
         </el-carousel-item>
       </el-carousel>
+      <div class="banner-content">
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item,index) in tabList"
+            :key="index"
+            class="tab"
+            @click="handleClick(index)"
+            :class="{active:current === index}"
+          >
+            <i>{{item.name}}</i>
+          </span>
+          <i></i>
+        </el-row>
+        <el-row type="flex" class="searchInform">
+          <input type="text" :placeholder="tabList[current].placeholder" class="search" />
+          <i class="el-icon-search icon-search" ></i>
+        </el-row>
+      </div>
     </div>
     <!-- 导入脚部组件 -->
     <Footer />
@@ -32,35 +50,122 @@ export default {
   data() {
     return {
       // 轮播图数据
-      banners: []
+      banners: [],
+      // tab栏数据
+      tabList: [
+        {
+          name: "攻略", //tab的名字
+          placeholder: "搜索城市" //搜索框提示文字
+        },
+        {
+          name: "酒店",
+          placeholder: "搜索你要搜索酒店"
+        },
+        {
+          name: "机票",
+          placeholder: "搜索机票"
+        }
+      ],
+      current: 0
     };
   },
-  mounted(){
+  methods: {
+    // 这里是tab栏点击切换
+    handleClick(index) {
+      // console.log("点击了" + index);
+      this.current = index;
+    }
+  },
+  mounted() {
+    // 这里是挂载完毕后，获取轮播图数据
     this.$axios({
-      url:'/scenics/banners',
-      method:'get'
-    }).then(res=>{
+      url: "/scenics/banners",
+      method: "get"
+    }).then(res => {
       console.log(res.data);
-      const {data} = res.data;
+      const { data } = res.data;
       this.banners = data;
-    })
+    });
   }
 };
 </script>
 
 <style scoped lang="less">
-.container{
-    min-width:1000px;
-    margin:0 auto;
-    position:relative;
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
-    /deep/ .el-carousel__container{
-        height:700px;
-    }
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
 
-    .banner-image{
-        width:100%;
-        height:100%;
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
+.banner-content {
+  position: absolute;
+  top: 30%;
+  left: 35%;
+  width: 600px;
+  z-index: 2;
+  .search-tab {
+    overflow: hidden;
+    .tab {
+      width: 80px;
+      height: 45px;
+      line-height: 45px;
+      text-align: center;
+      font-size: 18px;
+      background: rgba(0, 0, 0, 0.5);
+      // border: 1px solid red;
+      color: #fff;
+      position: relative;
+      margin-right: 10px;
+      border-radius: 2px;
+      &:after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 80px;
+        // background: pink;
+        width: 0px;
+        height: 0px;
+        border-bottom: 30px solid rgba(0, 0, 0, 0.5);
+        border-left: 12px solid rgba(0, 0, 0, 0.5);
+        border-right: 0px solid transparent;
+        border-top: 66px solid transparent;
+      }
     }
+  }
+  .search {
+    width: 522px;
+    height: 47px;
+    font-size: 18px;
+    text-indent: 1em;
+    border-radius: 4px;
+    border: none;
+    outline: none;
+  }
+}
+.searchInform{
+  position: relative;
+  .icon-search{
+    position: absolute;
+    top: 15px;
+    right: 90px;  
+    font-size: 20px;
+    font-weight: bold;
+  }
+}
+.active {
+  background: #eeeeee !important;
+  color:#000 !important;
+  &:after {
+    border-bottom: 30px solid #eeeeee !important;
+    border-left: 12px solid #eeeeee !important;
+  }
 }
 </style>
