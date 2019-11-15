052ca0915ca2bcb1df05f7adff05854b829208c4
caifakun <466411507@qq.com>
2019-11-15 17:32:50 +0800

    初始化订单页面，引入订单表单组件

---
+++ pages/air/order.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Header />
+    <div class="container">
+      <el-row type="flex" justify="space-between">
+        <!-- 订单表单 -->
+        <div class="main">
+            <OrderForm/>
+        </div>
+
+        <!-- 侧边栏 -->
+        <div class="aside"></div>
+      </el-row>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
+import OrderForm from "@/components/air/orderForm.vue"
+export default {
+  components: {
+    Header,
+    Footer,
+    OrderForm
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+/*aside*/
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
+</style>
