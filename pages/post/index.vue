a1933ab74a2273bf7b6bc1666f304099b49b62bd
caifakun <466411507@qq.com>
2019-11-18 14:17:43 +0800

    实现tab栏渲染数据和样式调整

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -3,35 +3,30 @@
     <Header />
     <div class="container">
       <div class="aside">
-        <ul class="tabs">
-          <li class="tabs-item">
-            热门城市
-            <span class="el-icon-arrow-right"></span>
-            <div class="item-content">
-              <ul>
-                <li>
-                  <i>1</i>
-                  <span class="city">北京</span>
-                  <span class="city-description">213213123</span>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li class="tabs-item">
-            推荐城市
-            <span class="el-icon-arrow-right"></span>
-          </li>
-          <li class="tabs-item">
-            奔向海岛
-            <span class="el-icon-arrow-right"></span>
-          </li>
-          <li class="tabs-item">
-            主题推荐
-            <span class="el-icon-arrow-right"></span>
-          </li>
-        </ul>asdad
+        <!-- tab栏切换 -->
+          <ul class="tabs"
+          @mouseleave="leave()">
+            <li
+              class="tabs-item"
+              v-for="(item,index) in cities"
+              :key="index"
+              :class="{right: current == index}"
+              @mouseover="over(index)"
+            >
+              {{item.type}}
+              <span class="el-icon-arrow-right color"></span>
+              <div class="item-content" :class="{show:current == index}" v-if="isShow">
+                <ul>
+                  <li v-for="(element,index) in item.children" :key="index">
+                    <i>{{index+1}}</i>
+                    <span class="city">{{element.city}}</span>
+                    <span class="city-description">{{element.desc}}</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
       </div>
-
       <div class="main"></div>
     </div>
 
@@ -42,15 +37,40 @@
 <script>
 import Header from "@/components/header.vue";
 import Footer from "@/components/footer.vue";
-import TabsItem from "@/components/post/tabs-item.vue";
 export default {
   components: {
     Header,
     Footer,
-    TabsItem
   },
   data() {
-    return {};
+    return {
+      cities: [], //用于渲染tabs栏数据
+      current: null, // 用于切换tabs数据
+      isShow: false //用于
+    };
+  },
+  methods: {
+    // 鼠标移入事件
+    over(index) {
+      this.current = index;
+      this.isShow = true;
+    },
+    // 鼠标移出事件
+    leave() {
+      this.isShow = false;
+      this.current = null;
+    }
+  },
+  mounted() {
+    // 实例挂载完毕后，进行请求城市菜单数据
+    this.$axios({
+      url: "/posts/cities",
+      method: "get"
+    }).then(res => {
+      console.log(res.data);
+      const { data } = res.data;
+      this.cities = data;
+    });
   }
 };
 </script>
@@ -65,39 +85,44 @@ export default {
   padding-top: 20px;
   .aside {
     width: 260px;
-    // height: 160px;
-    // background: pink;
     .tabs {
-      border: 1px solid #ddd;
       position: relative;
       .tabs-item {
+        border: 1px solid #ddd;
         padding: 0 20px;
         height: 40px;
         line-height: 40px;
         font-size: 14px;
-        border-bottom: 1px solid #ddd;
-        &:hover{
+        border-bottom: 0;
+        &:hover,
+        &:hover .color {
           color: #ffa500;
         }
         &:last-child {
-          border-bottom: 0;
+          border-bottom: 1px solid #ddd;
         }
         .el-icon-arrow-right {
           float: right;
           line-height: 40px;
           font-size: 20px;
+          color: #ddd;
         }
       }
     }
     .item-content {
+      display: none;
       width: 350px;
-      height: 180px;
+      background: #fff;
       border: 1px solid #ddd;
       position: absolute;
-      top: -1px;
+      top: 0px;
       right: -350px;
+      z-index: 1;
       li {
         color: #ffa500;
+        height: 40px;
+        line-height: 40px;
+        padding-bottom:  15px;
         i {
           padding-left: 20px;
           font-style: italic;
@@ -112,5 +137,17 @@ export default {
       }
     }
   }
+}
+.show {
+  display: block !important;
+  border-left: 0 !important;
+  li {
+    &:last-child {
+      border-left: 1px solid #ddd !important;
+    }
+  }
+}
+.right {
+  border-right: 0 !important;
 }
 </style>
