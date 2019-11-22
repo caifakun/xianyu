ba6cd4f789aaa4b54193507cb036e446b052fffe
caifakun <466411507@qq.com>
2019-11-22 10:13:32 +0800

    初始化地图组件

---
+++ components/hotel/map.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.onLoad = function() {
+      var map = new AMap.Map("container");
+    };
+    var url ="https://webapi.amap.com/maps?v=1.4.15&key=您申请的key值&callback=onLoad";
+    var jsapi = document.createElement("script");
+    jsapi.charset = "utf-8";
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#container {
+  width: 420px;
+  height: 260px;
+}
+</style>
+
